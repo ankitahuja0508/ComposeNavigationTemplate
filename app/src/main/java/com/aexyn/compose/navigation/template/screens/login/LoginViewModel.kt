909aa0d5f537cc2b033759b3e75a3fe79cc7de80
ankitahuja007@gmail.com <ankitahuja007@gmail.com>
2023-05-16 00:57:55 +0530

    added state in viewmodels

--- app/src/main/java/com/aexyn/compose/navigation/template/screens/login/LoginViewModel.kt
+++ app/src/main/java/com/aexyn/compose/navigation/template/screens/login/LoginViewModel.kt
@@ -2,6 +2,9 @@ package com.aexyn.compose.navigation.template.screens.login
 
 import android.app.Application
 import android.content.SharedPreferences
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.DefaultLifecycleObserver
 import com.aexyn.compose.navigation.template.base.BaseViewModel
 import com.aexyn.compose.navigation.template.extensions.saveModel
@@ -9,11 +12,17 @@ import com.aexyn.compose.navigation.template.screens.dashboard.User
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
+data class LoginState(
+    val isLoading: Boolean = false,
+)
 @HiltViewModel
 class LoginViewModel @Inject constructor(
     app: Application,
     private val sharedPreferences: SharedPreferences,
     ) : BaseViewModel(app), DefaultLifecycleObserver {
+
+    var state by mutableStateOf(LoginState())
+        private set
 
     fun login(){
         sharedPreferences.saveModel("USER_KEY", User("Test User", "test@email.com", "https://picsum.photos/200/200?random=11"))
