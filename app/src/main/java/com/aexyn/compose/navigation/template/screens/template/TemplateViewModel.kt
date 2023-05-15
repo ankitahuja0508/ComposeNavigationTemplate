bf1b6200c72704d8ba775a1948ab65b0eacc6112
ankitahuja007@gmail.com <ankitahuja007@gmail.com>
2023-05-16 01:12:44 +0530

    added state in template

--- app/src/main/java/com/aexyn/compose/navigation/template/screens/template/TemplateViewModel.kt
+++ app/src/main/java/com/aexyn/compose/navigation/template/screens/template/TemplateViewModel.kt
@@ -1,12 +1,23 @@
 package com.aexyn.compose.navigation.template.screens.template
 
 import android.app.Application
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.DefaultLifecycleObserver
 import com.aexyn.compose.navigation.template.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
+data class TemplateState(
+    val isLoading : Boolean = false
+)
+
 @HiltViewModel
 class TemplateViewModel @Inject constructor(
     app: Application,
-) : BaseViewModel(app), DefaultLifecycleObserver {}
+) : BaseViewModel(app), DefaultLifecycleObserver {
+
+    var state by mutableStateOf(TemplateState())
+        private set
+}
