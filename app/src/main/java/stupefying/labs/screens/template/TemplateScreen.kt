23be5bde1a787eeb5907b7b7dfcd5b8ac57c2165
ankitahuja007@gmail.com <ankitahuja007@gmail.com>
2023-05-20 15:16:53 +0530

    added text in template screen

--- app/src/main/java/stupefying/labs/screens/template/TemplateScreen.kt
+++ app/src/main/java/stupefying/labs/screens/template/TemplateScreen.kt
@@ -1,16 +1,17 @@
 package stupefying.labs.screens.template
 
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
-import stupefying.labs.MainViewModel
-import stupefying.labs.components.ScreenBody
-import stupefying.labs.extensions.activityViewModel
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.annotation.RootNavGraph
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+import stupefying.labs.MainViewModel
+import stupefying.labs.components.ScreenBody
+import stupefying.labs.extensions.activityViewModel
 
 @RootNavGraph(start = true)
 @Destination
@@ -27,6 +28,7 @@ fun TemplateScreen(
 fun TemplateView(
 ){
     ScreenBody(modifier = Modifier.fillMaxSize()) {
+        Text("Template Screen")
     }
 }
 
